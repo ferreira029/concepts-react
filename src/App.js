fc62f264f05ec984edbbc1708119492684c04f53
Marc Ferreira <msoftware.marc@outlook.com>
2020-04-08 02:31:56 -0300

    List, Create and Delete Data of repositories and test with JestJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setRepositories(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", { 
+      title: `aprendendo-react - ${Date.now()}`,
+      url: "https://github.com/ferreira029/aprendendo-react",
+      techs: ['React', 'React Native', 'VueJs', 'TypeScript'], 
+    });
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    repositories.splice(repositoryIndex, 1);
+    setRepositories([...repositories]);
+    return response.status;
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        { repositories.map(repository => <li key={repository.id}>
+          { repository.title }
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
